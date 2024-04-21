72dcc77f00ea3bb21be4a8645d3ee812a3a82b1a
sohaib-iot <ggsm.ecom@gmail.com>
2024-04-21 12:46:19 +0100

    Update clintes.c

--- Part-2/clintes.c
+++ Part-2/clintes.c
@@ -1,170 +1,84 @@
-#include "common.h"
+#include "cliente.h"
 
-void checkExistsFifoServidor_C1(char *nameFifo) {
-    so_debug("< [@param nameFifo:%s]", nameFifo);
-
-    // Check if FIFO server exists
-    if (access(nameFifo, F_OK) == -1) {
-        so_error("O FIFO do Servidor não existe.");
+// Implementação das funções do cliente
+void checkExistsFifoServidor_C1(char *serverFifo) {
+    struct stat st;
+    if (stat(serverFifo, &st) != 0 || !S_ISFIFO(st.st_mode)) {
+        so_error("C1", "");
         exit(EXIT_FAILURE);
+    } else {
+        so_success("C1", "");
     }
-
-    so_debug(">");
 }
 
 void triggerSignals_C2() {
-    so_debug("<");
-
-    // Arm and handle signals
-    if (signal(SIGUSR1, trataSinalSIGUSR1_C8) == SIG_ERR || 
-        signal(SIGHUP, trataSinalSIGHUP_C9) == SIG_ERR || 
-        signal(SIGINT, trataSinalSIGINT_C10) == SIG_ERR || 
-        signal(SIGALRM, trataSinalSIGALRM_C11) == SIG_ERR) {
-        so_error("Erro ao armar os sinais.");
+    if (signal(SIGUSR1, trataSinalSIGUSR1_C8) == SIG_ERR || signal(SIGHUP, trataSinalSIGHUP_C9) == SIG_ERR ||
+        signal(SIGINT, trataSinalSIGINT_C10) == SIG_ERR || signal(SIGALRM, trataSinalSIGALRM_C11) == SIG_ERR) {
+        so_error("C2", "");
         exit(EXIT_FAILURE);
+    } else {
+        so_success("C2", "");
     }
-
-    so_debug(">");
 }
 
-CheckIn getAuthenticationData_C3() {
-    so_debug("<");
-
-    CheckIn request;
-    request.nif = -1; // Default value indicates error
-
-    // Prompt user to enter NIF
+CheckIn getDadosPedidoUtilizador_C3_C4() {
+    CheckIn clientRequest;
     printf("IscteFlight: Check-in Online\n");
     printf("----------------------------\n");
     printf("Introduza o NIF do passageiro: ");
-    if (scanf("%d", &request.nif) != 1 || request.nif < 0 || request.nif > 999999999) {
-        so_error("NIF inválido. Introduza um NIF válido (até 9 dígitos).");
-        exit(EXIT_FAILURE);
-    }
-
-    // Prompt user to enter password
+    so_geti(&(clientRequest.nif));
     printf("Introduza a Senha do passageiro: ");
-    scanf("%s", request.senha);
+    so_gets(clientRequest.senha, sizeof(clientRequest.senha));
 
-    so_debug("> [@return nif:%d, senha:%s]", request.nif, request.senha);
-    return request;
-}
+    // Preenche o PID do cliente
+    clientRequest.pidCliente = getpid();
 
-CheckIn createClientRequest_C4(int pidCliente, CheckIn authData) {
-    so_debug("< [@param pidCliente:%d, authData.nif:%d, authData.senha:%s]", pidCliente, authData.nif, authData.senha);
-
-    CheckIn clientRequest;
-    clientRequest.nif = authData.nif;
-    strcpy(clientRequest.senha, authData.senha);
-    clientRequest.pidCliente = pidCliente;
-
-    so_success("Dados do cliente:", clientRequest.nif, clientRequest.senha, clientRequest.pidCliente);
-
-    so_debug("> [@return clientRequest.nif:%d, clientRequest.senha:%s, clientRequest.pidCliente:%d]", 
-                clientRequest.nif, clientRequest.senha, clientRequest.pidCliente);
+    so_success("C4", "%d %s %d", clientRequest.nif, clientRequest.senha, clientRequest.pidCliente);
     return clientRequest;
 }
 
-void writeRequest_C5(CheckIn request, char *nameFifo) {
-    so_debug("< [@param request.nif:%d, request.senha:%s, request.pidCliente:%d, nameFifo:%s]",
-                                        request.nif, request.senha, request.pidCliente, nameFifo);
-
-    // Open FIFO server
-    int fd = open(nameFifo, O_WRONLY);
-    if (fd == -1) {
-        perror("Erro ao abrir o FIFO do Servidor para escrita");
+void writeRequest_C5(CheckIn request, char *serverFifo) {
+    int fd;
+    if ((fd = open(serverFifo, O_WRONLY)) == -1) {
+        so_error("C5", "");
         exit(EXIT_FAILURE);
     }
 
-    // Write client request to FIFO server
-    if (write(fd, &request, sizeof(CheckIn)) == -1) {
-        perror("Erro ao escrever no FIFO do Servidor");
+    dprintf(fd, "%d\n%s\n%d\n", request.nif, request.senha, request.pidCliente);
+
+    if (close(fd) == -1) {
+        so_error("C5", "");
         exit(EXIT_FAILURE);
+    } else {
+        so_success("C5", "");
     }
-
-    close(fd);
-    so_success("Dados enviados para o servidor:", request.nif, request.senha, request.pidCliente);
-
-    so_debug(">");
 }
 
-void configureTimer_C6(int tempoEspera) {
-    so_debug("< [@param tempoEspera:%d]", tempoEspera);
-
-    alarm(tempoEspera);
-    so_success("Espera resposta em", tempoEspera, "segundos");
-
-    so_debug(">");
+void configureTimer_C6(int maxWaitTime) {
+    alarm(maxWaitTime);
+    so_success("C6", "Espera resposta em %d segundos", maxWaitTime);
 }
 
 void waitForEvents_C7() {
-    so_debug("<");
-
     pause();
-
-    so_debug(">");
 }
 
-void trataSinalSIGUSR1_C8(int sinalRecebido) {
-    so_debug("< [@param sinalRecebido:%d]", sinalRecebido);
-
-    so_success("Check-in concluído com sucesso");
+void trataSinalSIGUSR1_C8(int sig) {
+    so_success("C8", "Check-in concluído com sucesso");
     exit(EXIT_SUCCESS);
-
-    so_debug(">");
 }
 
-void trataSinalSIGHUP_C9(int sinalRecebido) {
-    so_debug("< [@param sinalRecebido:%d]", sinalRecebido);
-
-    so_success("Check-in concluído sem sucesso");
-    exit(EXIT_SUCCESS);
-
-    so_debug(">");
-}
-
-void trataSinalSIGINT_C10(int sinalRecebido) {
-    so_debug("< [@param sinalRecebido:%d]", sinalRecebido);
-
-    so_success("Cliente: Shutdown");
-    exit(EXIT_SUCCESS);
-
-    so_debug(">");
-}
-
-void trataSinalSIGALRM_C11(int sinalRecebido) {
-    so_debug("< [@param sinalRecebido:%d]", sinalRecebido);
-
-    so_error("Cliente: Timeout");
+void trataSinalSIGHUP_C9(int sig) {
+    so_success("C9", "Check-in concluído sem sucesso");
     exit(EXIT_FAILURE);
-
-    so_debug(">");
 }
 
-int main() {
-    // Constants
-    char *FIFO_SERVER = "server.fifo";
-    int MAX_ESPERA = 60;
+void trataSinalSIGINT_C10(int sig) {
+    so_success("C10", "Cliente: Shutdown");
+    exit(EXIT_SUCCESS);
+}
 
-    // C1: Validate FIFO server existence
-    checkExistsFifoServidor_C1(FIFO_SERVER);
-
-    // C2: Arm and handle signals
-    triggerSignals_C2();
-
-    // C3 + C4: Get authentication data and create client request
-    CheckIn authData = getAuthenticationData_C3();
-    int pidCliente = getpid();
-    CheckIn clientRequest = createClientRequest_C4(pidCliente, authData);
-
-    // C5: Write client request to FIFO server
-    writeRequest_C5(clientRequest, FIFO_SERVER);
-
-    // C6: Configure timer
-    configureTimer_C6(MAX_ESPERA);
-
-    // C7: Wait for events
-    waitForEvents_C7();
-
-    so_exit_on_error(-1, "ERRO: O cliente nunca devia chegar a este ponto");
+void trataSinalSIGALRM_C11(int sig) {
+    so_error("C11", "Cliente: Timeout");
+    exit(EXIT_FAILURE);
 }
